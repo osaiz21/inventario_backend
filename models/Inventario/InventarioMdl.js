de1a49655c41ffa53ba98bd84fb531df32060eef
oswald.saiz <oswald.saiz@lge.com>
2024-09-24 14:59:02 -0500

    Actualizando version

--- models/Inventario/InventarioMdl.js
+++ models/Inventario/InventarioMdl.js
@@ -29,19 +29,24 @@ const ListInventarioMdl = async ( {id_auditor = 0}) => {
     try {
         const results = await sequelize.query(`SELECT
             i.id, 
+            i.placa_nueva,
             b.codigo_plano,
             b.sede,
             b.departamento,
             b.ubicacion_fisica,
             b.piso,
-            i.placa_nueva,
             i.placa_antigua,
             i.nombre_activo,
             i.serie,
             i.createdAt,
-            i.fotos
+            i.fotos,
+            i.cantidad,
+            lm.marcas,
+            lmd.modelo
         from 
             inventario i left join ubicacion_inventario b on b.id = i.id_ubicacion
+                left join lista_marcas lm on i.id_marca = lm.id
+                left join lista_modelo lmd on i.id_modelo = lmd.id
         where 
             i.id_auditor  = ?`, {
             replacements: [id_auditor],
